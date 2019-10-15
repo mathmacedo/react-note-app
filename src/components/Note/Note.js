793c9638ce432711c8ec7d72737795c6eb37bc9f
Matheus Macedo <matheus.macedo1977@gmail.com>
2019-10-15 11:21:11 -0300

    Criando componente note

---
+++ src/components/Note/Note.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Note.css";
+import PropTypes from "prop-types";
+
+import Input from "../Input/Input";
+
+class Note extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	render() {
+		return <div></div>;
+	}
+}
+
+Note.propTypes = {};
+
+export default Note;
